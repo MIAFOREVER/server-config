9d8168b30bd7ae68af6f411c915fd671aa7fac4e
MIAFOREVER <15686357310@163.com>
2019-05-06 20:55:58 +0800

    update address

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sudo nohup sudo ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:9950  > "ssserverinfo.log" &
-sudo nohup ssmgr -c ~/server-config/config/ssmgr.yaml > "ssmanager.log" &
+sudo nohup sudo ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:6001  > "./log/ss-server-info.log" &
+sudo nohup ssmgr -c ~/server-config/config/ssmgr.yaml > "./log/ss-manager-info.log" &
